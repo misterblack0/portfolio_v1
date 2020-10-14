6edb34d70f9261139c8bc018710d3c2d1ed7bbbb
misterblack0 <misterblack0@protonmail.com>
2020-10-14 16:48:47 +0300

    refactor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import "./footer.scss";
 
 const Footer = () => (
-  <footer>
-    <p>© {new Date().getFullYear()} Marius Ciocoiu</p>
-  </footer>
+  <p className="footer">© {new Date().getFullYear()} Marius Ciocoiu</p>
 );
 
 export default Footer;
